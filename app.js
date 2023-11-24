1c937c05e7bc74c94e7f31f3efad8ff7fbd2235f
sikorskayaX <sikorskaya02@gmail.com>
2023-11-24 04:35:57 +0300

    Connecting the library to generate qr codes and the qr code generation function on the page

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener('DOMContentLoaded', function(){
+    alert('yes');
+    let qrcode = new QRCode(document.getElementById("qrcode"), {
+        width : 256,
+        height : 256
+    });
+
+
+
+
+    let makeCode = function () {		
+        let url = document.querySelector('input[type = "url"]');
+        
+        if (!url.value) {
+            alert("Input a url");
+            url.focus();
+            return;
+        }
+        
+        qrcode.makeCode(url.value);
+    }
+
+    document.querySelector('button').addEventListener('click', function(){
+        makeCode();
+    });
+
+});
