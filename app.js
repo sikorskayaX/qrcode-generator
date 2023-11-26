c9f763d87581fdac74cb6c3c1f12fd1dd80297d2
sikorskayaX <sikorskaya02@gmail.com>
2023-11-27 00:37:22 +0300

    Improve QR code error handling and UI updates
    This commit enhances the error handling mechanism in the QR code generation process by hiding the QR code image and clearing the canvas when an error occurs. It also refactors event listeners to use 'input' instead of 'keypress' and 'paste', streamlining the event handling for the URL input field. Additionally, the error message display has been simplified in the HTML, and the button styling has been improved with hover and focus effects. A LICENSE file for the QR code library has been added to comply with open source requirements.

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 ﻿document.addEventListener('DOMContentLoaded', function(){
     let errorMessage = document.querySelector(".error");
+    let qrcodeContainer = document.getElementById("qrcode");
     let url = document.querySelector('input[type = "url"]');
 
 
-    let qrcode = new QRCode(document.getElementById("qrcode"), {
+    let qrcode = new QRCode(qrcodeContainer, {
         width : 256,
         height : 256
     });
 
     let showError = function(){
-        errorMessage.classList.add("error-active")
+        errorMessage.classList.add("error-active");
+
+        let image = qrcodeContainer.querySelector('img');
+        image.style.display = 'none';
+        image.src = '';
+
+        let canvas = qrcodeContainer.querySelector('canvas');
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        canvas.style.display = 'block';
+        url.focus();
     }
 
     let hideError = function(){
-        errorMessage.classList.remove("error-active")
+        errorMessage.classList.remove("error-active");
     }
 
     let makeCode = function () {		
         
         if (!url.value) {
             showError();
-            url.focus();
             return;
         }
         
         qrcode.makeCode(url.value);
     }
 
-    url.addEventListener('keypress',  function(){
+    url.addEventListener('input',  function(){
         hideError();
     });
 
-    url.addEventListener('paste',  function(){
-        hideError();
-    });
 
     document.querySelector('button').addEventListener('click', function(){
         makeCode();
