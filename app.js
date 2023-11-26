0097f8aced23e5af2b3efc68b472ed2308facd1a
sikorskayaX <sikorskaya02@gmail.com>
2023-11-26 23:35:41 +0300

    Fix error handling in app.js and add error message in qrcode.html
    - Added error handling functionality in app.js to display an error message when the URL input is empty.
    - Added an error message element in qrcode.html to display the error message.
    - Updated the CSS to style the error message element.

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 ﻿document.addEventListener('DOMContentLoaded', function(){
-    alert('yes');
+    let errorMessage = document.querySelector(".error");
+    let url = document.querySelector('input[type = "url"]');
+
+
     let qrcode = new QRCode(document.getElementById("qrcode"), {
         width : 256,
         height : 256
     });
 
+    let showError = function(){
+        errorMessage.classList.add("error-active")
+    }
 
-
+    let hideError = function(){
+        errorMessage.classList.remove("error-active")
+    }
 
     let makeCode = function () {		
-        let url = document.querySelector('input[type = "url"]');
         
         if (!url.value) {
-            alert("Input a url");
+            showError();
             url.focus();
             return;
         }
         
         qrcode.makeCode(url.value);
     }
+
+    url.addEventListener('keypress',  function(){
+        hideError();
+    });
+
+    url.addEventListener('paste',  function(){
+        hideError();
+    });
 
     document.querySelector('button').addEventListener('click', function(){
         makeCode();
